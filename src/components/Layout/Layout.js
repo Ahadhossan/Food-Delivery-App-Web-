74411ba092312d708165ab07c85162d1e4175491
Md Ahad Hossain <ahadm3016@gmail.com>
2024-07-14 19:47:22 +0600

    first commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+import Footer from "../Footer/Footer.jsx";
+import Routes from "../../routes/Routers";
+
+import Carts from "../UI/cart/Carts.jsx";
+import { useSelector } from "react-redux";
+import Header from "../Header/Header.jsx";
+
+const Layout = () => {
+  const showCart = useSelector((state) => state.cartUi.cartIsVisible);
+  return (
+    <div>
+      <Header />
+
+      {showCart && <Carts />}
+
+      <div>
+        <Routes />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
